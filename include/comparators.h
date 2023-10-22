2eb881035dcf3b00ff5991c67af921dadbae747f
Navid Shamszadeh <nshamszadeh@gmail.com>
2023-10-22 13:02:49 -0700

    uhhhhh hahahaha

--- include/comparators.h
+++ include/comparators.h
@@ -7,25 +7,6 @@
 // recall opencv image pixels are stored in BGR format by default
 // TODO: Make BGR -> HLS conversion happen only once per image Mat (regardless of number of sorts)
 namespace comparator {
-  inline float get_hue(const cv::Vec3b& v1) {
-    auto minc_v1 = std::min(v1[0], v1[1], v1[2]);
-    auto maxc_v1 = std::max(v1[0], v1[1], v1[2]);
-
-    float v1_hue = 0.0;
-
-    if (minc_v1 < maxc_v1) { // minc_v1 == maxc_v1 => v1_hue == 0
-      auto diff_v1 = maxc_v1 - minc_v1;
-      auto red_v1 = (maxc_v1 - v1[2]) / diff_v1;
-      auto green_v1 = (maxc_v1 - v1[1]) / diff_v1;
-      auto blue_v1 = (maxc_v1 - v1[0]) / diff_v1;
-      if (red_v1 == maxc_v1) v1_hue = blue_v1 - green_v1;
-      else if (green_v1 == maxc_v1) v1_hue = 2.0 + red_v1 - blue_v1;
-      else v1_hue = 4.0 + green_v1 - red_v1;
-
-      v1_hue = std::fmod(v1_hue, 6.0);
-      return v1_hue;
-    }
-  }
 
   std::function<bool(const cv::Vec3b&, const cv::Vec3b&)> lightness = [](const cv::Vec3b& v1, const cv::Vec3b& v2) {
     //auto minc_v1 = std::min(v1[0], v1[1], v1[2]);
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
-#include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
+//#include <opencv2/core.hpp>
+//#include <opencv2/imgproc.hpp>
 //#include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include "util.h"
+#include "comparators.h"
+#include "sorter.h"
 
 #include <cstdint>
 #include <iostream>
@@ -16,47 +18,6 @@
 /*
 TODO: output a pixel-sorted video file
 */
-
-/**
- * Sorts pixels of frame using intervals from mask.
- * Sorting is based on the sorting method passed by sorter.
- * For now this is going to sort row-wise only.
- * 
- * Inputs:
- * 	cv::Mat frame - image to apply pixel sorting to
- * 	cv::Mat mask  - Binary image detailing the intervals to sort ()
- * 									Intervals are set to 1 and separated by 0
- */
-cv::Mat pixelSort(cv::Mat frame, cv::Mat mask, std::function<bool(const cv::Vec3b&, const cv::Vec3b&)> comp, bool rowWise = false) {
-	// frame and mask must be same dimensions
-	CV_Assert((frame.rows == mask.rows) && (frame.cols == mask.cols));
-	CV_Assert((frame.type() == CV_8UC3) && (mask.type() == CV_8UC1));
-
-	cv::Mat tmp = cv::Mat(frame.size(), frame.type());
-	if (!rowWise)
-		cv::rotate(frame, tmp, cv::ROTATE_90_COUNTERCLOCKWISE);
-	else
-		frame.copyTo(tmp);
-	// sort column-wise
-	// find an interval, sort based on the interval, go to next interval
-	constexpr int UNSET = -1;
-	int start = UNSET;
-	for (int i = 0; i < tmp.rows; i++) {
-		auto tmp_row = tmp.row(i);
-		auto mask_row = mask.row(i);
-		for (int j = 0; j < tmp_row.cols; j++) {
-			if (mask_row.at<uchar>(j) == 1 && start == UNSET) { // new interval has been reached
-				start = j; // set interval starting point
-			} else if ((mask_row.at<uchar>(j) == 0 || j == tmp_row.cols - 1) && start != UNSET) { // current interval has ended
-				int end = j - 1; // set current interval ending point. j - 1 is safe because j == 0 implies start == UNSET
-				std::sort(tmp_row.begin<cv::Vec3b>() + start, tmp_row.begin<cv::Vec3b>() + end, comp); // sort pixels
-				// reset starting and ending points
-				start = UNSET;
-			}
-		}
-	}
-	return tmp;
-}
 
 int main() {
 	std::string video_path = "/mnt/c/Users/xxtou/Videos/2022-12-28 13-48-08_Trim.mp4";
@@ -85,10 +46,13 @@ int main() {
 		test.at<cv::Vec3b>(0,2) = cv::Vec3b(5,4,3);
 		std::cout << test.at<cv::Vec3b>(2) << std::endl;
 		std::cout << std::endl << test << std::endl;
-		//cv::imshow("AHAHAHA", frame);
-		//output_vid.write(frame);
-		//cv::waitKey(30);
-	} while (/*!frame.empty()*/ 0 );
+		cv::Mat mask = cv::Mat::ones(frame.size(), CV_8U);
+		
+		cv::Mat sorted_frame = pixelSort(frame, mask, comparator::lightness);
+		output_vid.write(sorted_frame);
+		cv::imshow("AHAHAHA", sorted_frame);
+		cv::waitKey(30);
+	} while (!frame.empty());
 
 	return 0;
 }
