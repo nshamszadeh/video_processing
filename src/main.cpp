bc37849c4d893d70de0cd26e17963779d4ea0aaa
Navid <nshamszadeh@gmail.com>
2024-01-20 03:10:26 -0800

    more formatting

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 #include "maskgen.h"
 
 int main(int argc, char *argv[]) {
-	std::string video_path = static_cast<std::string>(argv[1]);
-	std::string mask_path;
-	if (argc > 2) mask_path = argv[2];
-	else mask_path = maskgen::bgSubtract(video_path);
-	fx::pixelSort(video_path, mask_path, comparator::maximum());
-	return 0;
+    std::string video_path = static_cast<std::string>(argv[1]);
+    std::string mask_path;
+    if (argc > 2) mask_path = argv[2];
+    else mask_path = maskgen::bgSubtract(video_path);
+    fx::pixelSort(video_path, mask_path, comparator::maximum());
+    return 0;
 }
